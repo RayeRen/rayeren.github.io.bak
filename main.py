8d0b6ba5daee2622bd582bf27530806e8161cf80
Yi Ren <rayeren613@gmail.com>
2022-02-22 11:27:17 +0800

    update

--- main.py
+++ main.py
@@ -4,16 +4,12 @@ import json
 from datetime import datetime
 import os
 
-scholar_ids = ['4FA6C0AAAAAJ']
-
-for id in scholar_ids:
-    author = scholarly.search_author_id(id)
-    scholarly.fill(author, sections=['basics', 'indices', 'counts', 'publications'])
-    name = author['name']
-    author['updated'] = str(datetime.now())
-    author['publications'] = {v['author_pub_id'].split(":")[1]:v for v in author['publications']}
-    print(json.dumps(author, indent=2))
-    # author = jsonpickle.encode(author)
-    os.makedirs('results', exist_ok=True)
-    with open(f'results/yiren.json', 'w') as outfile:
-        json.dump(author, outfile, ensure_ascii=False)
+author = scholarly.search_author_id(os.environ['GOOGLE_SCHOLAR_ID'])
+scholarly.fill(author, sections=['basics', 'indices', 'counts', 'publications'])
+name = author['name']
+author['updated'] = str(datetime.now())
+author['publications'] = {v['author_pub_id'].split(":")[1]:v for v in author['publications']}
+print(json.dumps(author, indent=2))
+os.makedirs('results', exist_ok=True)
+with open(f'results/gs_data.json', 'w') as outfile:
+    json.dump(author, outfile, ensure_ascii=False)
